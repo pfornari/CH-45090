882788a91ab4b174627b23c1f7edb7309b6f52e3
pfornari <pedro@fornari.org>
2023-01-22 23:56:13 -0300

    primera

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hola como estan?
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Documentación 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
